0a4b7c3cafbefa2ed0ba6ba108b48b914ac6a717
Diksha Ambuj Goyal <dikshaambujgoyal@Dikshas-MacBook-Air.local>
2026-08-27 17:18:13 +0530

    docs: remove redirects containing blackstrap

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // output: 'export',
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/blackstrap-molasses',
-        destination: '/premium-industrial-molasses',
-        permanent: true,
-      },
-      {
-        source: '/blog/what-is-blackstrap-molasses-procurement-guide',
-        destination: '/blog/what-is-sugarcane-molasses-procurement-guide',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
